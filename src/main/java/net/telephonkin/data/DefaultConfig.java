ba80cdcd5f19a51accb5c5311830a36c379d3872
Telephonkin <nick.telephonkin@gmail.com>
2026-06-27 14:01:45 +0200

    casting default config to hash

--- src/main/java/net/telephonkin/data/DefaultConfig.java
+++ src/main/java/net/telephonkin/data/DefaultConfig.java
@@ -1,15 +1,20 @@
 package net.telephonkin.data;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
 import com.google.gson.reflect.TypeToken;
 import net.fabricmc.loader.api.FabricLoader;
 import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.*;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class DefaultConfig {
     private static final Path CONFIG_PATH = FabricLoader.getInstance().getConfigDir();
@@ -17,7 +22,7 @@ public class DefaultConfig {
 
     public static DefaultConfig config = new DefaultConfig();
 
-    public Map loadConfig() throws IOException, URISyntaxException {
+    public Map<String, Integer> loadConfig() throws IOException, URISyntaxException {
         if (CONFIG.exists()) {
             // Read the existing entity_lifetime_config.json5 configuration file
             String map = CONFIG.toString();
@@ -28,22 +33,32 @@ public class DefaultConfig {
         } else {
             // Take file DefaultConfig.json5 from same directory and create it in config directory;
             // Use config from this DefaultConfig.json5 file
-            //File DefaultConfigFile = new File("DefaultConfig.json5");
             URL DefaultConfigFile = DefaultConfig.class.getResource("/DefaultConfig.json5");
-            assert DefaultConfigFile != null;
+
+            assert DefaultConfigFile != null; // Ensure that there is DefaultConfig.json5
+
             Path DefaultConfigFilePath = Paths.get(DefaultConfigFile.toURI());
             Gson gson = new Gson();
 
-            TypeToken<Map<String, Integer>> mapType = new TypeToken<Map<String, Integer>>(){};
-            //Map<String, Integer> default_vanilla_config_map = gson.fromJson(map, mapType);
-            Map default_vanilla_config_map = gson.fromJson(Files.readString(DefaultConfigFilePath), Map.class);
-                    //Type type = new TypeToken<HashMap<String, String>>(){}.getType();
-            //HashMap<String, Integer> myMap = new Gson().fromJson(map, map.getClass());
-            //HashMap<String,Integer> default_vanilla_config_map = new Gson().fromJson(
-            //        map,
-            //        new TypeToken<HashMap<String, Integer>>()  {}.getType()
-            //);
-            //Files.writeString(CONFIG_PATH, (CharSequence) default_vanilla_config_map);
+            Map default_vanilla_config_map_as_map = gson.fromJson(Files.readString(DefaultConfigFilePath), Map.class); // Use this Map as config for entities lifetime
+
+            Map<String, Integer> default_vanilla_config_map_unraw = (Map<String, Integer>) default_vanilla_config_map_as_map;
+
+
+            HashMap<String, Integer> default_vanilla_config_map = new HashMap<String, Integer>(default_vanilla_config_map_unraw);
+            //HashMap<String, Integer> hashMap =
+            //        (default_vanilla_config_map_unraw instanceof HashMap)
+            //                ? (HashMap) default_vanilla_config_map_unraw
+            //                : new HashMap<String, Integer>(default_vanilla_config_map_unraw);
+
+            //HashMap<String, Integer> default_vanilla_config_map = (HashMap<String, Integer>) default_vanilla_config_map_unraw;
+
+            //System.out.println(CONFIG_PATH.toString() + "/entity_lifetime_config.json5");
+
+            try (PrintWriter out = new PrintWriter(CONFIG_PATH.toString() + "/entity_lifetime_config.json5")) {
+                out.println(default_vanilla_config_map.toString());
+            }
+
             return default_vanilla_config_map;
         }
     }
