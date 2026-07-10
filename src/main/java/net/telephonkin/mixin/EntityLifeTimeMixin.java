85a811a9e9d318d56b6e544f9c9b4bc44e28861f
Telephonkin <nick.telephonkin@gmail.com>
2026-07-10 22:37:37 +0200

    cleanup, WIP

--- src/main/java/net/telephonkin/mixin/EntityLifeTimeMixin.java
+++ src/main/java/net/telephonkin/mixin/EntityLifeTimeMixin.java
@@ -5,8 +5,8 @@ import net.minecraft.server.world.ServerWorld;
 import net.minecraft.entity.Entity;
 import net.telephonkin.EntityLifeTimeMod;
 import net.telephonkin.data.EntityLifeTimeTable;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -24,8 +24,8 @@ public abstract class EntityLifeTimeMixin {
 
 	@Shadow @Final private MinecraftServer server;
 
-	@Unique
-	private static final Logger LOGGER = LoggerFactory.getLogger("entity-lifetime-mod");
+	//@Unique
+	//private static final Logger LOGGER = LoggerFactory.getLogger("entity-lifetime-mod");
 	@Unique
 	private static HashMap<String, Integer> LOADED_MOD_CONFIG = EntityLifeTimeMod.INSTANCE.getLoadedConfig();
 
@@ -36,22 +36,15 @@ public abstract class EntityLifeTimeMixin {
 
 			//Iterable<ServerWorld> worlds = server.getWorlds();
 			ServerWorld overworld = server.getOverworld();
-			//ServerWorld world = server.getOverworld();
-			//ServerWorld world = (ServerWorld) server.getWorld();
 
 			EntityLifeTimeTable entity_birth_table = EntityLifeTimeTable.get(overworld);
 			long birthdate;
 
 			String entityTypeString = entity.getType().toString().substring(7).replace(".",":");
-			//String entityTypeString = Registries.ENTITY_TYPE.getId(entity.getType()).toString();
-			LOGGER.info("ENTITY TYPE IS :{}", entityTypeString);
-			//LOGGER.info("LIFETIME:{}", LOADED_MOD_CONFIG.get(entityTypeString));
-			//LOGGER.info("LIFETIME:{}", (String) LOADED_MOD_CONFIG.get(entityTypeString).getClass().getName());
-			//System.out.println("ENTITY TYPE IS :" + entityTypeString);
-			//int not_spawn = -1;
-			//LOADED_MOD_CONFIG.keySet().getClass().getTypeName();
+
+			//LOGGER.info("ENTITY TYPE IS :{}", entityTypeString);
 			if (((Number) LOADED_MOD_CONFIG.get(entityTypeString)).intValue() != -1) {
-				// write data about entity UUID and birthtime to the table
+				// Write data about entity UUID and birth time to the table
 				birthdate = server.getTicks();
 
 				entity_birth_table.setMap(putProperly(
@@ -60,9 +53,6 @@ public abstract class EntityLifeTimeMixin {
 						entity,
 						entity.getUuid(),
 						birthdate));
-				//entity_birth_table.entityLifeTimeTable.put(entity.getUuid(), birthdate);
-				//System.out.println("Table:" + entity_birth_table.getMap());
-				//System.out.println("A Creeper has spawned with UUID:" + entity.getUuidAsString() + "at " + birthdate);
 				entity_birth_table.markDirty();
 			}
 
